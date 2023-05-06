e373c4061db81cbbc6fbae4bdc3477386796dcd5
Benjamin Froelich <bbenif@gmail.com>
2023-05-06 21:06:48 +0200

    forgot to add converters for serialization.

---
+++ OptimalWeekApp/OptimalWeekApp/OptimalWeekApp/Models/Converters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace OptimalWeekApp.Models
+{
+    public static class ColorConverter
+    {
+        public static int ToInt(Xamarin.Forms.Color color)
+        {
+            int a = (int)(color.A * 255);
+            int r = (int)(color.R * 255);
+            int g = (int)(color.G * 255);
+            int b = (int)(color.B * 255);
+            return (a << 24) | (r << 16) | (g << 8) | b;
+        }
+
+        public static Xamarin.Forms.Color FromInt(int argb)
+        {
+            int a = (argb >> 24) & 0xFF;
+            int r = (argb >> 16) & 0xFF;
+            int g = (argb >> 8) & 0xFF;
+            int b = argb & 0xFF;
+            return Xamarin.Forms.Color.FromRgba(r, g, b, a);
+        }
+
+    }
+
+    public static class DayTimeConverter
+    {
+        public static int toMinutes(DayTime dt)
+        {
+            return dt.toMinutes;
+        }
+
+        public static DayTime fromMinutes(int minutes)
+        {
+            return new DayTime(minutes);
+        }
+    }
+
+}
---
+++ OptimalWeekApp/OptimalWeekApp/OptimalWeekApp/Services/EventStore.cs
@@ -0,0 +1,87 @@
+﻿using OptimalWeekApp.Models;
+using OptimalWeekApp.Services;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+public class EventStore : IDataStore<ITimeSpanEvent>
+{
+    private readonly SQLiteAsyncConnection _connection;
+    private readonly string _db_path = "EventStore.db";
+
+    public EventStore()
+    {
+        var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), _db_path);
+
+        // temporary to delete the database on restart
+        if (File.Exists(databasePath))
+        {
+            File.Delete(databasePath);
+        }
+
+        _connection = new SQLiteAsyncConnection(databasePath);
+        _connection.CreateTableAsync<WeeklyEvent>().Wait();
+    }
+
+    public async Task<IEnumerable<ITimeSpanEvent>> GetEventsWeekday(string weekday)
+    {
+        WeekDay day;
+        Enum.TryParse<WeekDay>(weekday, out day);
+
+        return await _connection.Table<WeeklyEvent>().Where(e => e.Day == day).ToListAsync();
+    }
+
+    public async Task<bool> AddItemAsync(ITimeSpanEvent item)
+    {
+        if (item is WeeklyEvent weeklyEvent)
+        {
+            await _connection.InsertAsync(weeklyEvent);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteItemAsync(string id)
+    {
+        var item = await GetItemAsync(id);
+        if (item != null)
+        {
+            await _connection.DeleteAsync(item);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public async Task<ITimeSpanEvent> GetItemAsync(string id)
+    {
+        return await _connection.FindAsync<WeeklyEvent>(id);
+    }
+
+    public async Task<IEnumerable<ITimeSpanEvent>> GetItemsAsync(bool forceRefresh = false)
+    {
+        return await _connection.Table<WeeklyEvent>().ToListAsync();
+    }
+
+    public async Task<bool> UpdateItemAsync(ITimeSpanEvent item)
+    {
+        if (item is WeeklyEvent weeklyEvent)
+        {
+            await _connection.UpdateAsync(weeklyEvent);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
